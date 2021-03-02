145212aee608f1b6b05bb9f217cd03c7e9ab7c2f
Monmon Saha <saham2@upmc.edu>
2021-03-01 21:14:44 -0500

    updated 4 methods for search

--- search_compare.py
+++ search_compare.py
@@ -1,6 +1,5 @@
 import argparse
 # other imports go here
-
 import random
 
 def get_me_random_list(n):
@@ -13,21 +12,61 @@ def get_me_random_list(n):
     random.shuffle(a_list)
     return a_list
     
-def sequential_search(a_list,item):
-    pass
+def sequential_search(a_list, item):
+  pos = 0
+  found = False
+  while pos < len(a_list) and not found:
+    if a_list[pos] == item:
+      found = True
+    else:
+      pos = pos + 1
+  return found
 
 
-def ordered_sequential_search(a_list,item):
-    pass
+def ordered_sequential_search(a_list, item):
+  pos = 0
+  found = False
+  stop = False
+  while pos < len(a_list) and not found and not stop:
+    if a_list[pos] == item:
+      found = True
+    else:
+      if a_list[pos] > item:
+        stop = True
+      else:
+        pos = pos + 1
+  return found
 
 
 def binary_search_iterative(a_list,item):
-    pass
-    
-    
-def binary_search_recursive(a_list,item):
-    pass
+    first = 0
+    last = len(a_list) - 1
+    found = False
+    while first <= last and not found:
+   
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+          if item < a_list[midpoint]:
+            last = midpoint - 1
+          else:
+            first = midpoint + 1
+    return found   
+  
 
+def binary_search_recursive(a_list, item):
+  if len(a_list) == 0:
+    return False
+  else:
+    midpoint = len(a_list) // 2
+    if a_list[midpoint] == item:
+      return True
+    else:
+      if item < a_list[midpoint]:
+        return binary_search(a_list[:midpoint], item)
+      else:
+        return binary_search(a_list[midpoint + 1:], item)
 
 if __name__ == "__main__":
     """Main entry point"""
